e853b280c1f63b46b35498628155bf71a9f3fa2e
HanumanTheHacker <111376466+HanumanTheHacker@users.noreply.github.com>
2023-03-05 22:05:16 +0530

    y

--- main.js
+++
@@ -1,9 +0,0 @@
-function openbar(){
-    let sidebar = document.getElementById("sidebar");
-    sidebar.style.left = "0";
-}
-
-function closebar(){
-    let sidebar = document.getElementById("sidebar");
-    sidebar.style.left = "-250px";
-}
